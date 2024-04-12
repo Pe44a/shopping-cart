28983af4bc1b486b8395cff35a8906389a5ac918
Peteris <blankfeldspeteris@gmail.com>
2024-04-12 21:32:06 +0300

    Cart component: - Renders shopping cart items fetched from localStorage - Displays items - Provides "Checkout" button to clear localStorage and show success alert

--- shopping-cart-react/src/components/Cart.jsx
+++ shopping-cart-react/src/components/Cart.jsx
@@ -1,15 +1,58 @@
 import NavBar from "./NavBar"
+import { Link } from "react-router-dom"
 
 function Cart() {
+  const cartItems = JSON.parse(localStorage.getItem('itemData'));
+
+  function handleCheckout() {
+    let data = JSON.parse(localStorage.getItem('itemData'))
+    data = [];
+    localStorage.setItem('itemData', JSON.stringify(data));
+
+    alert("Checkout successful");
+  }
 
     return (
       <>
         <NavBar/>
-  
-        <div className='main'>
-          List of added items
-        </div>
+
+        {cartItems.length < 1 ? (
+            <div className="empty-cart">
+              <h2>Your cart is empty</h2>
+              <Link to="/shop">Shop</Link>
+            </div>
+          ) : (
+            <> 
+            <div className="cart-items">
+              {cartItems.map((product) => (
+                <>
+                  <div className="check-out-container">
+                    <div className="check-out-img">
+                      <img src={product.image} alt="" />
+                    </div>
+                    <div className="check-out-description">
+                      <div>
+                        <div>{product.title}</div>
+                        <div>{product.category}</div>
+                      </div>
+                      <div>
+                        <div>Price: {product.price}$</div>
+                        <div>Amount: {product.amount}</div>
+                      </div>
+                    </div>
+                  </div>
+                </>
+              ))}
+            </div>
+
+            <div className="check-out-btn-container">
+              <button className="checkout-btn" onClick={() => handleCheckout()} >Checkout</button>
+            </div>
+
+            </>
+          )}
       
+          
       </>
     )
   }
