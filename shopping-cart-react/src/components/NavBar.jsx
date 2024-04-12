311f6df95a853cd0a5a7078aa417485baadf2276
Peteris <blankfeldspeteris@gmail.com>
2024-04-12 22:31:42 +0300

    NavBar component: - Track if items are present in cart - Render checkmark icon next to "Cart" link if items are present

--- shopping-cart-react/src/components/NavBar.jsx
+++ shopping-cart-react/src/components/NavBar.jsx
@@ -1,16 +1,28 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import checkMarkIcon from '../assets/check-mark.png';
 
 function NavBar() {
-    
+  const [itemAdded, setItemAdded] = useState("");
+
+  useEffect(() => {
+    const localData = JSON.parse(localStorage.getItem("itemData")) || [];
+    if (localData.length > 0) {
+      setItemAdded(checkMarkIcon);
+    }
+  }, []);
 
     return (
     <>
-      <div className='header'>
-        <div className='shop-name'>Some-shop</div>
-        <div className='nav-bar'>
+      <div className="header">
+        <div className="shop-name">Some-shop</div>
+        <div className="nav-bar">
           <Link to="/">Home</Link>
           <Link to="/shop">Shop</Link>
-          <Link to="/cart">Cart</Link>
+          <Link to="/cart">
+            Shop
+           <img src={itemAdded} alt="" /> 
+          </Link>
         </div>
       </div>
     </>
